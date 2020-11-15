f13da51835be2892c5f7dabb4d0b9b7c2eee2191
learner-web <73613683+learner-web@users.noreply.github.com>
2020-11-15 23:10:44 +0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,20 @@
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+const uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult(authResult, redirectUrl) {
+      return true;
+    },
+    uiShown() {
+      document.getElementById('loader').style.display = 'none';
+    },
+  },
+  signInFlow: 'popup',
+  signInSuccessUrl: 'signedIn',
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+};
+ui.start('#firebaseui-auth-container', uiConfig);
+© 2020 GitHub, Inc.
